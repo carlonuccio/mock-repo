74e86a9f2e5f18adddf932ad4f542461856cd8a6
Carlo Nuccio <carlonuccio91@gmail.com>
2022-10-11 11:49:32 +0200

    add code in files types: .tf remove code in files types: .tf,.md,.yaml,.py,.toml,.cfg,.txt

--- content.tf
+++ content.tf
@@ -2,4 +2,4 @@ resource "random_string" "tdiwx" { length = 10 }
 resource "random_string" "fvqxm" { length = 10 }
 resource "random_string" "ohyjy" { length = 10 }
 resource "random_string" "ghruu" { length = 10 }
-resource "random_string" "xqudt" { length = 10 }
+resource "random_string" "javrs" { length = 10 }
