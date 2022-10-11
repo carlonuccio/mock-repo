cb1f5df1a4d4eb2569a7846e53283c6872c2b470
Carlo Nuccio <carlonuccio91@gmail.com>
2022-10-11 10:45:37 +0200

    add code in files types: .tf,.txt,.md,.yaml,.py,.toml,.cfg remove code in files types: .tf,.txt,.md,.yaml,.py,.toml,.cfg

--- content.tf
+++ content.tf
@@ -2,4 +2,4 @@ resource "random_string" "tdiwx" { length = 10 }
 resource "random_string" "fvqxm" { length = 10 }
 resource "random_string" "ohyjy" { length = 10 }
 resource "random_string" "ghruu" { length = 10 }
-resource "random_string" "ipfuy" { length = 10 }
+resource "random_string" "xqudt" { length = 10 }
