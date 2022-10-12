4d375160afb0ad8d1d77ef9a41474d3f62b30132
Carlo Nuccio <carlonuccio91@gmail.com>
2022-10-12 18:52:27 +0200

    add code in files types: .tf,.txt, remove code in files types: .tf,.txt

--- content.tf
+++ content.tf
@@ -1,4 +1,4 @@
 resource "random_string" "tdiwx" { length = 10 }
 resource "random_string" "fvqxm" { length = 10 }
 resource "random_string" "alrxs" { length = 10 }
-resource "random_string" "dplmw" { length = 10 }
+resource "random_string" "pcyoy" { length = 10 }
