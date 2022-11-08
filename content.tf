c8c5235c431279869fb36690e3a8a3801eb6e3e1
Carlo Nuccio <carlonuccio91@gmail.com>
2022-11-08 22:38:44 +0100

    add code in files types: .hcl,.yaml,.json,.tf remove code in files types: .tf

--- content.tf
+++ content.tf
@@ -1,4 +1,4 @@
 resource "random_string" "tdiwx" { length = 10 }
 resource "random_string" "fvqxm" { length = 10 }
 resource "random_string" "alrxs" { length = 10 }
-resource "random_string" "lkhfi" { length = 10 }
+resource "random_string" "vwjoa" { length = 10 }
