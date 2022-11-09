157a1327a33d2f62813e2fec6eb97393b295c54b
Carlo Nuccio <carlonuccio91@gmail.com>
2022-11-09 17:02:57 +0100

    add code in files types: .hcl,.tf remove code in files types: .hcl

--- content.tf
+++ content.tf
@@ -2,3 +2,4 @@ resource "random_string" "tdiwx" { length = 10 }
 resource "random_string" "fvqxm" { length = 10 }
 resource "random_string" "alrxs" { length = 10 }
 resource "random_string" "vwjoa" { length = 10 }
+resource "random_string" "msnhx" { length = 10 }
