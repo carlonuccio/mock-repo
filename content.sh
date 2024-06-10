fa2d2a81f7d601082eda8f9b7b197b319bf066ef
Carlo Nuccio <carlonuccio91@gmail.com>
2024-06-10 11:13:34 +0200

    add code in files types: .yml,.sql,.sh remove code in files types: .sql,.sh

--- content.sh
+++ content.sh
@@ -1,2 +1,2 @@
 echo "reblk"
-echo "fdygn"
+echo "ixlep"
--- content.sql
+++ content.sql
@@ -130,4 +130,4 @@ SELECT * from bvxht;
 SELECT * from bjhgb;
 SELECT * from vhojn;
 SELECT * from uaiyh;
-SELECT * from cjlmm;
+SELECT * from tfqwq;
