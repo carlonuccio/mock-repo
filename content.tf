ead9374b80d0f6ca08aeb55f3867da84ad370e78
Carlo Nuccio <carlonuccio91@gmail.com>
2024-06-19 16:06:14 +0200

    add code in files types: .tf,.hcl remove code in files types: .tf,.hcl

--- content.tf
+++ content.tf
@@ -20,4 +20,4 @@ resource "random_string" "auwmd" { length = 10 }
 resource "random_string" "vwurs" { length = 10 }
 resource "random_string" "rrese" { length = 10 }
 resource "random_string" "obnng" { length = 10 }
-resource "random_string" "xhuik" { length = 10 }
+resource "random_string" "hlgij" { length = 10 }
