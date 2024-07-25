c84c7490d9a3a79a70477c207e8c4c1e496806c5
Carlo Nuccio <carlonuccio91@gmail.com>
2024-07-25 14:26:05 +0200

    add code in files types: .tf remove code in files types:

--- content.tf
+++ content.tf
@@ -23,3 +23,4 @@ resource "random_string" "obnng" { length = 10 }
 resource "random_string" "fsbqa" { length = 10 }
 resource "random_string" "mddhw" { length = 10 }
 resource "random_string" "qgttw" { length = 10 }
+resource "random_string" "nwjqu" { length = 10 }
