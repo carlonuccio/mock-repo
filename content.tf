6fd9bc4830d521ee27aa1bd94d385a1bfe89b763
Carlo Nuccio <carlonuccio91@gmail.com>
2024-11-22 10:16:07 +0100

    add code in files types: .hcl,.tf,.md,,.txt remove code in files types: .hcl,.tf,.md

--- content.tf
+++ content.tf
@@ -29,4 +29,4 @@ resource "random_string" "pajox" { length = 10 }
 resource "random_string" "eejis" { length = 10 }
 resource "random_string" "hjark" { length = 10 }
 resource "random_string" "awltq" { length = 10 }
-resource "random_string" "opejc" { length = 10 }
+resource "random_string" "lqbfo" { length = 10 }
